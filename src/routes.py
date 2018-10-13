1e42bdb913dd93242b0fa0f4794501e42c4ebc65
root <root@LAPTOP-JCD3LNVF.localdomain>
2018-10-13 13:36:17 -0400

    Passed all tests

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,11 @@
 import json
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 post_id_counter = 2
+
+comment_id_counter = [1, 0]
 
 posts = {
   0: {
@@ -11,7 +13,7 @@ posts = {
     'score': 0,
     'text': 'My First Post!',
     'username': 'Young',
-  }
+  },
   1: {
     'id': 1,
     'score': 0,
@@ -19,6 +21,18 @@ posts = {
     'username': 'Young'
   }
 }
+
+comments = {
+  0: [
+      { 
+        'id': 0,
+        'score': 0,
+        'text': 'My First Comment!',
+        'username': 'Young'
+      }
+  ],
+  1:[]
+}   
 
 @app.route("/")
 def hello():
@@ -44,13 +58,16 @@ def create_post():
     'username': username
   }
   posts[post_id_counter] = post
+  comment_id_counter.append(0)
+  comments[post_id_counter] = []
+  post_id_counter += 1
   return json.dumps({'success': True, 'data': post}), 201
 
 @app.route('/api/post/<int:post_id>/')
 def get_post(post_id):
   if post_id in posts:
     post = posts[post_id]
-    return json.dumps({'success':True, 'data':post}), 200
+    return json.dumps({'success':True, 'data': post}), 200
   return json.dumps({'success': False, 'error': 'Post not fouund'}),404
 
 @app.route('/api/post/<int:post_id>/', methods=['POST'])
@@ -67,8 +84,32 @@ def delete_post(post_id):
   if post_id in posts:
     post = posts[post_id]
     del posts[post_id]
-    return json.dumps({'success': True, 'data': post})
+    return json.dumps({'success': True, 'data': post}), 200
   return json.dumps({'success': False, 'error': 'Post not found'}), 404
 
+@app.route('/api/post/<int:post_id>/comments/')
+def get_comments(post_id):
+  if post_id in posts:
+    comment_list = comments[post_id]
+    return json.dumps({'success': True, 'data': comment_list}), 200
+  return json.dumps({'success': False, 'error': 'Post not found'}), 404
+
+@app.route('/api/post/<int:post_id>/comment/', methods=['POST'])
+def post_comment(post_id):
+  if post_id in posts:
+    global comment_id_counter
+    post_body = json.loads(request.data)
+    comment_id = comment_id_counter[post_id]
+    comment = {
+      'id': comment_id,
+      'score': 0,
+      'text': post_body['text'],
+      'username': post_body['username']
+    }
+    comments[post_id].append(comment)
+    comment_id_counter[post_id] += 1
+    return json.dumps({'success': True, 'data': comment}), 201
+  return json.dumps({'success': False, 'error': 'Post not found'}), 404
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
