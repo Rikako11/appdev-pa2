4d97abb4c3779464ae982e222de338e080d56fd1
root <root@LAPTOP-JCD3LNVF.localdomain>
2018-10-14 10:01:08 -0400

    Fixed spacing

--- src/routes.py
+++ src/routes.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 post_id_counter = 2
 
-comment_id_counter = [1, 0]
+comment_id_counter = {0: 1, 1: 0}
 
 posts = {
   0: {
     'id': 0,
     'score': 0,
     'text': 'My First Post!',
-    'username': 'Young',
+    'username': 'Young'
   },
   1: {
     'id': 1,
@@ -58,7 +58,7 @@ def create_post():
     'username': username
   }
   posts[post_id_counter] = post
-  comment_id_counter.append(0)
+  comment_id_counter[post_id_counter] = 0
   comments[post_id_counter] = []
   post_id_counter += 1
   return json.dumps({'success': True, 'data': post}), 201
@@ -84,6 +84,8 @@ def delete_post(post_id):
   if post_id in posts:
     post = posts[post_id]
     del posts[post_id]
+    del comments[post_id]
+    del comment_id_counter[post_id]
     return json.dumps({'success': True, 'data': post}), 200
   return json.dumps({'success': False, 'error': 'Post not found'}), 404
 
@@ -97,7 +99,6 @@ def get_comments(post_id):
 @app.route('/api/post/<int:post_id>/comment/', methods=['POST'])
 def post_comment(post_id):
   if post_id in posts:
-    global comment_id_counter
     post_body = json.loads(request.data)
     comment_id = comment_id_counter[post_id]
     comment = {
@@ -110,6 +111,11 @@ def post_comment(post_id):
     comment_id_counter[post_id] += 1
     return json.dumps({'success': True, 'data': comment}), 201
   return json.dumps({'success': False, 'error': 'Post not found'}), 404
+
+# For debugging purposes
+@app.route('/api/posts/comments/')
+def get_all_comments():
+  return json.dumps({'success': True, 'data': comments}), 201
     
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
